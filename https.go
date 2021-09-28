71b318bcc407caefbab3789c154e9b9c7c35b4dd
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2021-09-28 11:39:04 -0700

    https proxy mvp

--- https.go
+++ https.go
@@ -2,4 +2,37 @@ package main
 
 // Some code Copyright (c) 2020 Andrew Ayer, under X11, MIT-advertising license
 
-import ()
+import (
+	"log"
+	"net/http/httputil"
+	"net/url"
+	"os"
+
+	"github.com/caddyserver/certmagic"
+	"github.com/libdns/cloudflare"
+)
+
+func httpsProxy() {
+
+	token := os.Getenv("CLOUDFLARE_TOKEN")
+	if token == "" {
+		log.Fatalln("env var CLOUDFLARE_TOKEN is not set")
+	}
+
+	certmagic.DefaultACME.DNS01Solver = &certmagic.DNS01Solver{
+		DNSProvider: &cloudflare.Provider{
+			APIToken: token,
+		},
+	}
+
+	url, err := url.Parse("http://localhost:999")
+	if err != nil {
+		log.Fatalln("url.parse", err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	err = certmagic.HTTPS([]string{"*.sfu1.com"}, proxy)
+	log.Fatalln("certmagic.https", err)
+
+}
